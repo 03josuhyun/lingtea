0616ee51f23180a2d72d2c1df5ae21c5bd335294
03josuhyun <a01027541583@gmail.com>
2026-09-21 18:15:04 +0900

    four_commit01

---
+++ src/data/BannerImg.js
@@ -0,0 +1,32 @@
+const banner = [
+  {
+    id: 'banner01',
+    image : process.env.PUBLIC_URL + '/assets/banner/banner01.webp',
+  },
+  {
+    id: 'banner02',
+    image : process.env.PUBLIC_URL + '/assets/banner/banner02.webp',
+  },
+  {
+    id: 'banner03',
+    image : process.env.PUBLIC_URL + '/assets/banner/banner03.webp',
+  },
+  {
+    id: 'banner04',
+    image : process.env.PUBLIC_URL + '/assets/banner/banner04.webp',
+  },
+  {
+    id: 'banner05',
+    image : process.env.PUBLIC_URL + '/assets/banner/banner05.jpg',
+  },
+  {
+    id: 'banner06',
+    image : process.env.PUBLIC_URL + '/assets/banner/banner06.webp',
+  },
+  {
+    id: 'banner07',
+    image : process.env.PUBLIC_URL + '/assets/banner/banner07.webp',
+  },
+]
+
+export default banner;
